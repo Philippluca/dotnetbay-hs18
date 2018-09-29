62470ec331eaf9a97c040d852b05b5542337ce9a
Philipp Lüthi <philippluca.luethi@gmail.com>
2018-09-29 12:10:03 +0200

    Lab02 Application startup & Main Windows

---
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.Entity;
+using DotNetBay.Data.Provider.FileStorage;
+using DotNetBay.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IMainRepository mainRepository; 
+        private readonly IAuctionRunner auctionRunner;
+
+        public IMainRepository MainRepository
+        {
+            get => mainRepository;
+        }
+
+        public App()
+        {
+            mainRepository = new FileSystemMainRepository(".\\persitance");
+            auctionRunner = new AuctionRunner(mainRepository);
+            this.auctionRunner.Start();
+            InitializeTestData();
+        }
+
+        private void InitializeTestData()
+        {
+            var memberService = new SimpleMemberService(this.MainRepository);
+            var service = new AuctionService(this.MainRepository, memberService);
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+    }
+}
