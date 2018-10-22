7ed55a78e110c6a4af11102e55ab0f88f76324f7
Philipp Lüthi <philipp@luethi.rocks>
2018-10-22 18:08:30 +0200

    Completed Lab05 up to 2.2

---
+++ source/DotNetBay.Data.EF/MainDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DotNetBay.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace DotNetBay.Data.EF
+{
+    class MainDbContext : DbContext
+    {
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Member> Members { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+
+        }
+
+
+    }
+}
